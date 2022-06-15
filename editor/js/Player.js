b415628b8221933bc3a0ec5f92af48f68a2de318
Muhammad Naeem Akhtar <naeem.cs533@gmail.com>
2022-06-14 23:55:28 -0700

    Updated Player.js
    Show/Hide Modal on Play and Stop button

--- editor/js/Player.js
+++ editor/js/Player.js
@@ -28,7 +28,7 @@ function Player( editor ) {
 
 	signals.startPlayer.add( function () {
 		let scene = editor.scene;
-
+		scene.visible =false;
 		
 		const loader = new THREE.CubeTextureLoader();
 		const texture = loader.load(skyBoxImageListOnPlay);
@@ -42,6 +42,7 @@ function Player( editor ) {
 
 	signals.stopPlayer.add( function () {
 		let scene = editor.scene;
+		scene.visible =true;
 		const loader = new THREE.CubeTextureLoader();
 		const texture = loader.load(skyBoxImagesList);
 		texture.mapping = THREE.CubeRefractionMapping;
